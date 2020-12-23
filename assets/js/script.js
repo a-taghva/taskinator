9fc51603cf1738027df935436cfa518b0b6d0589
Ali Taghva <taghvali99@gmail.com>
2020-12-22 22:46:43 -0500

    add tasks with content dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,27 @@ let formEl = document.querySelector("#task-form");
 
 let createTaskHandler = function(event) {
     event.preventDefault();
-
+    
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector(".select-dropdown").value;
+    
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    console.log(taskInfoEl);
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list 
     tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
