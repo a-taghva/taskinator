ad8af121cd54d7f552d468a8bc75e95e297deb18
Ali Taghva <taghvali99@gmail.com>
2020-12-22 21:14:51 -0500

    add submit eventListener to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 let buttonEl = document.querySelector("#save-task");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
+let formEl = document.querySelector("#task-form");
 
-let createTaskHandler = function() {
+let createTaskHandler = function(event) {
+    event.preventDefault();
+
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
